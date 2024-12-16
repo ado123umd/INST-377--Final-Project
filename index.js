f4a3ce6f4024a6f755359af186dfee64f241a5c5
ado123umd <143612927+ado123umd@users.noreply.github.com>
2024-12-16 16:42:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const scheduleRoutes = require('./src/routes/scheduleRoutes');
 // Middleware
 app.use(cors());
 app.use(express.json()); 
+app.get('/', (req, res) => {
+    res.sendFile('public/home.html', { root: 'public' });
+});
 
 //register cours routes
 app.use('/api/courses', courseRoutes);
