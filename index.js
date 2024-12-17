044ff0a602889986427e0bfa020712c384cea5f1
Andy Do <ado123@terpmail.umd.edu>
2024-12-17 02:11:29 -0500

    adding in app.use(express.static(__dirname + '/public')); on the index file

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(__dirname + '/public'));
+
 // Serve the home page
 app.get('/', (req, res) => {
     res.sendFile('home.html', { root: path.join(__dirname, 'public') });
